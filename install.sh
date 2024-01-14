b906184c2c932e081a34b59f980b5996f32fcdc3
Naverite <56008568+Naverite@users.noreply.github.com>
2024-01-13 21:49:58 -0600

    Updated install.sh
    Added script portions to script installing node exporter to the raspberry

--- install.sh
+++ install.sh
@@ -12,4 +12,25 @@ rm log2ram.tar.gz
 sudo systemctl daemon-reload
 sudo systemctl enable log2ram
 sudo systemctl restart log2ram
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-armv7.tar.gz
+tar -xvzf node_exporter-1.7.0.linux-armv7.tar.gz
+sudo cp node_exporter-1.7.0.linux-armv7/node_exporter /usr/local/bin
+sudo chmod +x /usr/local/bin/node_exporter
+sudo useradd -m -s /bin/bash node_exporter
+sudo mkdir /var/lib/node_exporter
+sudo chown -R node_exporter:node_exporter /var/lib/node_exporter
+sudo cd /etc/systemd/system/
+echo "[Unit]
+Description=Node Exporter
+
+[Service]
+# Provide a text file location for https://github.com/fahlke/raspberrypi_exporter data with the
+# --collector.textfile.directory parameter.
+ExecStart=/usr/local/bin/node_exporter --collector.textfile.directory /var/lib/node_exporter/textfile_collector
+
+[Install]
+WantedBy=multi-user.target" > node_exporter.service
+sudo systemctl daemon-reload 
+sudo systemctl enable node_exporter.service
+sudo systemctl restart node_exporter.service
 
